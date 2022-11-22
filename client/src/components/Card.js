a3ecedb52ca0994182fe29de82fe64b4fd855f21
Dina <dinamshawky@gmail.com>
2022-11-21 21:12:50 -0800

    responsive layout

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,5 +1,6 @@
 import {
   Flex,
+  SimpleGrid,
   Box,
   Image,
   useColorModeValue,
@@ -19,208 +20,159 @@ const data = {
 function ProductAddToCart() {
   return (
     <Flex p={10} w="full" alignItems="center" justifyContent="space-between">
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        maxW="sm"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-        margin={5}
-        padding={2}
-      >
-        <Image
-          src={data.imageURL}
-          alt={`Picture of ${data.name}`}
-          roundedTop="lg"
-        />
-
-        <Box p="6" space={100}>
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="l"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {data.name}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1em"}
-            >
-              <chakra.a href={"#"} display={"flex"}>
-                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
-              </chakra.a>
-            </Tooltip>
-          </Flex>
-
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
-              <Box as="span" color={"gray.600"} fontSize="lg">
-                $
+      <SimpleGrid columns={{ base: 1, lg: 4, md: 3 }} spacing={5}>
+        <Box
+          bg={useColorModeValue("white", "gray.800")}
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          shadow="lg"
+          position="relative"
+          margin={5}
+          padding={2}
+        >
+          <Image
+            src={data.imageURL}
+            alt={`Picture of ${data.name}`}
+            roundedTop="lg"
+          />
+          <Box p="6">
+            <Flex mt="1" justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="l"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {data.name}
               </Box>
-              {data.price.toFixed(2)}
-            </Box>
-          </Flex>
-        </Box>
-      </Box>
+              <Tooltip
+                label="Add to cart"
+                bg="white"
+                placement={"top"}
+                color={"gray.800"}
+                fontSize={"1em"}
+              >
+                <chakra.a href={"#"} display={"flex"}>
+                  <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
+                </chakra.a>
+              </Tooltip>
+            </Flex>
 
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        maxW="sm"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-        margin={5}
-        padding={2}
-      >
-        <Image
-          src={data.imageURL}
-          alt={`Picture of ${data.name}`}
-          roundedTop="lg"
-        />
-
-        <Box p="6">
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="l"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {data.name}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1em"}
-            >
-              <chakra.a href={"#"} display={"flex"}>
-                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
-              </chakra.a>
-            </Tooltip>
-          </Flex>
-
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
-              <Box as="span" color={"gray.600"} fontSize="lg">
-                $
+            <Flex justifyContent="space-between" alignContent="center">
+              <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
+                <Box as="span" color={"gray.600"} fontSize="lg">
+                  $
+                </Box>
+                {data.price.toFixed(2)}
               </Box>
-              {data.price.toFixed(2)}
-            </Box>
-          </Flex>
+            </Flex>
+          </Box>
         </Box>
-      </Box>
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        maxW="sm"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-        margin={5}
-        padding={2}
-      >
-        <Image
-          src={data.imageURL}
-          alt={`Picture of ${data.name}`}
-          roundedTop="lg"
-        />
 
-        <Box p="6">
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="l"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {data.name}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1em"}
-            >
-              <chakra.a href={"#"} display={"flex"}>
-                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
-              </chakra.a>
-            </Tooltip>
-          </Flex>
+        <Box
+          bg={useColorModeValue("white", "gray.800")}
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          shadow="lg"
+          position="relative"
+          margin={5}
+          padding={2}
+        >
+          <Image
+            src={data.imageURL}
+            alt={`Picture of ${data.name}`}
+            roundedTop="lg"
+          />
 
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
-              <Box as="span" color={"gray.600"} fontSize="lg">
-                $
+          <Box p="6">
+            <Flex mt="1" justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="l"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {data.name}
               </Box>
-              {data.price.toFixed(2)}
-            </Box>
-          </Flex>
-        </Box>
-      </Box>
+              <Tooltip
+                label="Add to cart"
+                bg="white"
+                placement={"top"}
+                color={"gray.800"}
+                fontSize={"1em"}
+              >
+                <chakra.a href={"#"} display={"flex"}>
+                  <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
+                </chakra.a>
+              </Tooltip>
+            </Flex>
 
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        maxW="sm"
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-        margin={5}
-        padding={2}
-      >
-        <Image
-          src={data.imageURL}
-          alt={`Picture of ${data.name}`}
-          roundedTop="lg"
-        />
-
-        <Box p="6">
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="l"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {data.name}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1em"}
-            >
-              <chakra.a href={"#"} display={"flex"}>
-                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
-              </chakra.a>
-            </Tooltip>
-          </Flex>
-
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
-              <Box as="span" color={"gray.600"} fontSize="lg">
-                $
+            <Flex justifyContent="space-between" alignContent="center">
+              <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
+                <Box as="span" color={"gray.600"} fontSize="lg">
+                  $
+                </Box>
+                {data.price.toFixed(2)}
               </Box>
-              {data.price.toFixed(2)}
-            </Box>
-          </Flex>
+            </Flex>
+          </Box>
         </Box>
-      </Box>
+
+        <Box
+          bg={useColorModeValue("white", "gray.800")}
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          shadow="lg"
+          position="relative"
+          margin={5}
+          padding={2}
+        >
+          <Image
+            src={data.imageURL}
+            alt={`Picture of ${data.name}`}
+            roundedTop="lg"
+          />
+
+          <Box p="6">
+            <Flex mt="1" justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="l"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {data.name}
+              </Box>
+              <Tooltip
+                label="Add to cart"
+                bg="white"
+                placement={"top"}
+                color={"gray.800"}
+                fontSize={"1em"}
+              >
+                <chakra.a href={"#"} display={"flex"}>
+                  <Icon as={FiShoppingCart} h={7} w={7} alignSelf={"center"} />
+                </chakra.a>
+              </Tooltip>
+            </Flex>
+
+            <Flex justifyContent="space-between" alignContent="center">
+              <Box fontSize="l" color={useColorModeValue("gray.800", "white")}>
+                <Box as="span" color={"gray.600"} fontSize="lg">
+                  $
+                </Box>
+                {data.price.toFixed(2)}
+              </Box>
+            </Flex>
+          </Box>
+        </Box>
+      </SimpleGrid>
     </Flex>
   );
 }
