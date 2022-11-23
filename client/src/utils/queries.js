ecdd84f2f8047934da98d12a4611af8bf9d89d3b
YeshNels <paleojosh@sbcglobal.net>
2022-11-22 20:43:42 -0800

    Trying again

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,89 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
+export const QUERY_CATEGORIES = gql`
+  query category {
+    categories {
       _id
       name
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PRODUCT = gql`
+  query getProduct($product: ID) {
+    product {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query getUser {
+    user {
+      firstName
+      lastName
+      email
+      orders {
+        _id
+        purchasedate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_ORDER = gql`
+  query getorder($order: ID) {
+    order {
+      _id
+      purchasedate
+      products {
+        _id
+        name
+        description
+        price
+        quantity
+        image
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getcheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
     }
   }
 `;
