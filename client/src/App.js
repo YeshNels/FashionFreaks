7074301be8993d29312d71f6decd25b7c27a4864
Nilu <nirali.rkapadia@gmail.com>
2022-11-23 19:31:41 -0800

    Commented the router and apollo client imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import ProductAddToCart from "./components/Card";
-import SignIn from "./components/signIn";
+//import SignIn from "./components/signIn";
 
 function App() {
   return (
