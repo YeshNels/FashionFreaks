54a1fd6c1d66fe50f9df6e7e723c6c4e83f9aef2
Dina <dinamshawky@gmail.com>
2022-11-26 10:09:13 -0800

    organize the pages and edit the sign in and sign up

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Card from "../components/Card";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Navbar />
+      <Header />
+      <Card />
+    </div>
+  );
+};
+
+export default Home;
