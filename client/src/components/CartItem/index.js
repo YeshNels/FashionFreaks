402a541263d352b92dc9e0d43c3f2d2a0bdcab73
Dina <dinamshawky@gmail.com>
2022-11-28 01:25:27 -0800

    edit

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -56,7 +56,7 @@ const CartItem = ({ item }) => {
           size="sm"
           objectFit="cover"
           maxW={{ base: "100%", sm: "150px" }}
-          src="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80"
+          src={`/images/${item.image}`}
           alt={item.name}
         />
 
