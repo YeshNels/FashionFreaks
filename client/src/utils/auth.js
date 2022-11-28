525662e8b7f9941ed80b2a23b388c65ccde28692
Dina <dinamshawky@gmail.com>
2022-11-28 03:20:46 -0800

    edit the auth file

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,4 +1,10 @@
+import decode from "jwt-decode";
+
 class AuthService {
+  getProfile() {
+    return decode(this.getToken());
+  }
+
   loggedIn() {
     const token = this.getToken();
     return token ? true : false;
