a514ef6551a4c4a64d4f626c35637a874fd8fab2
baileyrachael25 <baileyrachael25@gmail.com>
2022-11-28 17:32:29 -0800

    small changes to seeds, server file, cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -10,11 +10,24 @@ import {
 } from "@chakra-ui/react";
 import { FiShoppingCart } from "react-icons/fi";
 
-const data = {
+/*const data = {
   imageURL:
     "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
   name: "Sunglasses Classic",
   price: 4.5,
+};*/
+
+const data = {
+  products: [
+    {
+      "name": "Ray-Ban Clubmaster Classic Sunglasses",
+      "description": "Black frame with green lenses",
+      "image": "/public/images/ray-ban-clubmaster.jpg",
+      "price": 163,
+      "quantity": "",
+      "category": "Sunglasses"
+    },
+  ],
 };
 
 function ProductAddToCart() {
